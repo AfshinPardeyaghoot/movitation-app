de98f602c862ee7d8d94b6f055f2e2b03ac91cfc
afshinpht1379@gmail.com <afshinpht1379@gmail.com>
2022-01-12 19:18:55 +0330

    add notFoundException class

---
+++ src/main/java/com/doit/doitplatform/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.doit.doitplatform.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
