5435e93b0017db048dc68e57f27531bde8297516
afshinpht1379@gmail.com <afshinpht1379@gmail.com>
2022-01-12 19:19:18 +0330

    add UserRepository class

---
+++ src/main/java/com/doit/doitplatform/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.doit.doitplatform.repository;
+
+import com.doit.doitplatform.base.repository.AbstractRepository;
+import com.doit.doitplatform.model.User;
+
+public interface UserRepository extends AbstractRepository<User, Long> {
+}
