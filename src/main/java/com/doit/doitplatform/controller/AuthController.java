1151a4b22503f6dd7d3aa489b4598add1e9bbe2c
afshinpht1379@gmail.com <afshinpht1379@gmail.com>
2022-01-15 20:12:28 +0330

    add service to create user

--- src/main/java/com/doit/doitplatform/controller/AuthController.java
+++ src/main/java/com/doit/doitplatform/controller/AuthController.java
@@ -1,25 +1,21 @@
 package com.doit.doitplatform.controller;
 
 import com.doit.doitplatform.dto.SignUpDTO;
-import com.doit.doitplatform.exception.NotFoundException;
 import com.doit.doitplatform.model.Category;
 import com.doit.doitplatform.model.PageStyle;
-import com.doit.doitplatform.model.Role;
 import com.doit.doitplatform.model.User;
 import com.doit.doitplatform.service.CategoryService;
 import com.doit.doitplatform.service.UserService;
 import lombok.SneakyThrows;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,7 +23,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 
 @Controller
 public class AuthController {
@@ -36,32 +31,23 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final CategoryService categoryService;
     private final UserService userService;
+    private final String BASE_URL;
 
     @Autowired
-    public AuthController(PasswordEncoder passwordEncoder, UserService userService, AuthenticationManager authenticationManager, CategoryService categoryService) {
+    public AuthController(@Value("${application.base.url}") String BASE_URL, PasswordEncoder passwordEncoder, UserService userService, AuthenticationManager authenticationManager, CategoryService categoryService) {
         this.passwordEncoder = passwordEncoder;
         this.userService = userService;
         this.authenticationManager = authenticationManager;
         this.categoryService = categoryService;
+        this.BASE_URL = BASE_URL;
     }
 
+    @SneakyThrows
     @PostMapping("/sign-up")
     public String signUp(SignUpDTO signUpDTO, HttpServletRequest request) {
-        User user = new User();
-        user.setUsername(signUpDTO.getUsername());
-        user.setEmail(signUpDTO.getEmail());
-        user.setPassword(passwordEncoder.encode(signUpDTO.getPassword()));
-        user.setRoles(new ArrayList<Role>() {{
-            add(Role.ROLE_USER);
-        }});
-        User u = userService.save(user);
-        try {
-            request.login(signUpDTO.getUsername(), signUpDTO.getPassword());
-        } catch (Exception e) {
-            throw new NotFoundException("");
-        }
 
-
+        User u = userService.createUser(signUpDTO);
+        request.login(signUpDTO.getUsername(), signUpDTO.getPassword());
         return "redirect:/index";
     }
 
@@ -76,7 +62,7 @@ public class AuthController {
     @ModelAttribute("pageStyle")
     public PageStyle style() {
         PageStyle style = new PageStyle();
-        style.setBackgroundImage("http://localhost:8083/image/ai");
+        style.setBackgroundImage(BASE_URL + "/image/ai");
         return style;
     }
 
@@ -91,32 +77,11 @@ public class AuthController {
         return "login";
     }
 
-//    @GetMapping("/image/{imageName}")
-//    public ResponseEntity<?> getImage(@PathVariable String imageName) {
-//
-//        Resource resource = getFileResource("/home/afshinpy/iam/image.jpg");
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.IMAGE_PNG)
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + "image" + "\"")
-//                .body(resource);
-//    }
-//
-//    public Resource getFileResource(String fileUri) {
-//        try {
-//            Resource resource = new UrlResource(fileUri);
-//            if (resource.exists())
-//                return resource;
-//            else throw new NotFoundException(String.format("file with path %s not found", fileUri));
-//        } catch (MalformedURLException e) {
-//            System.out.println("herre");
-//        }
-//        return null;
-//    }
 
     @SneakyThrows
     @GetMapping("/image/{imageName}")
-    public void getImageAsByteArray(HttpServletResponse response) throws IOException {
-        File file = new File("/home/afshinpy/iam/image.jpg");
+    public void getImageAsByteArray(HttpServletResponse response, @PathVariable String imageName) throws IOException {
+        File file = new File(String.format("/home/afshinpy/iam/%s", imageName));
         InputStream in = new FileInputStream(file);
         response.setContentType(MediaType.IMAGE_JPEG_VALUE);
         IOUtils.copy(in, response.getOutputStream());
--- src/main/java/com/doit/doitplatform/service/UserService.java
+++ src/main/java/com/doit/doitplatform/service/UserService.java
@@ -1,29 +1,48 @@
 package com.doit.doitplatform.service;
 
 import com.doit.doitplatform.base.service.BaseServiceImpl;
+import com.doit.doitplatform.dto.SignUpDTO;
 import com.doit.doitplatform.exception.NotFoundException;
+import com.doit.doitplatform.model.Role;
 import com.doit.doitplatform.model.User;
 import com.doit.doitplatform.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class UserService extends BaseServiceImpl<User, Long, UserRepository> implements UserDetailsService {
 
-    private UserRepository repository;
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Autowired
-    public UserService(UserRepository abstractRepository) {
+    public UserService(UserRepository abstractRepository, PasswordEncoder passwordEncoder) {
         super(abstractRepository);
         this.repository = abstractRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByUsername(username)
                 .orElseThrow(() -> new NotFoundException(String.format("User by username %s not found", username)));
+    }
+
+    public User createUser(SignUpDTO signUpDTO) {
+        User user = new User();
+        user.setUsername(signUpDTO.getUsername());
+        user.setEmail(signUpDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(signUpDTO.getPassword()));
+        user.setRoles(new ArrayList<Role>() {{
+            add(Role.ROLE_USER);
+        }});
+        return save(user);
     }
 }
